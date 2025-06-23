4ffc5bcc90bee72140981e3f385286d51b22b675
aarush1023 <63690978+aarush1023@users.noreply.github.com>
2025-06-23 12:25:59 -0700

    hid all secret keys and api keys

--- keys.py
+++ keys.py
@@ -1,3 +1,3 @@
-MONGO_URI = "sample uri"
-SECRET_KEY = "sample secret key"
-ALGORITHM = "sample algorithm"
+MONGO_URI = "mongodb+srv://aarush:Kv097f8P1vXZdM65@cluster0.nxphn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+SECRET_KEY = "L3UyFYhcsv2LxbDHxJTUTII3mjTKwKw4"
+ALGORITHM = "HS256"
